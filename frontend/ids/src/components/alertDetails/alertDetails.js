bda62c1ee0a36ef04c2ee85885eab6c7862f673a
Manash07 <manashghimire2072@gmail.com>
2025-12-22 13:38:56 +0000

    Minimal changes in the development

--- frontend/ids/src/components/alertDetails/alertDetails.js
+++ frontend/ids/src/components/alertDetails/alertDetails.js
@@ -19,7 +19,7 @@ export default function AlertDetails() {
       <table className="table">
         <thead>
           <tr key="New">
-            <th>Unique ID</th>
+            <th>Reference Number</th>
             <th>Source IP</th>
             <th>Alert</th>
             <th>Timestamp</th>
@@ -42,23 +42,47 @@ export default function AlertDetails() {
                 <td>{new Date(alert.createdAt).toLocaleString()}</td>{" "}
                 {/* Assuming createdAt is the timestamp field */}
                 <td>
-                  {" "}
-                  <span className="badge rounded-pill text-bg-danger">
-                    Unresolved
-                  </span>{" "}
+                  <OverlayTrigger
+                    placement="top"
+                    overlay={
+                      <Tooltip id="unresolved-tooltip">
+                        This issue is yet to be solved. If it is resolved click
+                        Resolved button in the Action column.
+                      </Tooltip>
+                    }
+                  >
+                    <span className="d-inline-block">
+                      <span className="badge rounded-pill text-bg-danger">
+                        Unresolved
+                      </span>
+                    </span>
+                  </OverlayTrigger>
                 </td>
                 <td>
                   {" "}
-                  <button type="button" className="btn btn-danger">
-                    {" "}
-                    <i className="bi bi-trash-fill"></i> Delete{" "}
-                  </button>{" "}
+                  <OverlayTrigger
+                    placement="top"
+                    overlay={
+                      <Tooltip id="unresolved-tooltip">
+                        Click to delete the alert from the record permanently.
+                      </Tooltip>
+                    }
+                  >
+                    <span>
+                      <button type="button" className="btn btn-danger">
+                        {" "}
+                        <i className="bi bi-trash-fill"></i> Delete{" "}
+                      </button>{" "}
+                    </span>
+                  </OverlayTrigger>
                   <span>
                     <OverlayTrigger
                       placement="top"
                       overlay={
-                        <Tooltip>
-                          This function is still under built. Will be out soon. Visit updates section for more details.
+                        <Tooltip style={{color:"red"}}>
+                         ! *** This function is still under development and is
+                          expected to be out soon. Visit updates section for
+                          more details *** !
                         </Tooltip>
                       }
                     >
@@ -72,14 +96,14 @@ export default function AlertDetails() {
                       placement="top"
                       overlay={
                         <Tooltip>
-                        Since it is icmp
-                        ping flood attack. The administrator is suggested to
-                        close the port to prevent further attack
+                          Since it is icmp ping flood attack. The administrator
+                          is suggested to close the port to prevent further
+                          attack
                         </Tooltip>
                       }
                     >
                       <button className="btn btn-warning">
-                        <i className="bi bi-lightbulb-fill"></i>  Get tips
+                        <i className="bi bi-lightbulb-fill"></i> Get tips
                       </button>
                     </OverlayTrigger>
                   </span>
